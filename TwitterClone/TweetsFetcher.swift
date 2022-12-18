91694cf77633d049bfbca0b1ce4ee00ee5b8ff58
Alexey Mihailovsky <alexey.mihailovsky@gmail.com>
2022-12-18 21:44:58 +0100

    feat: add tweets fetcher

---
+++ TwitterClone/TweetsFetcher.swift
@@ -0,0 +1,36 @@
+//
+//  TweetsFetcher.swift
+//  TwitterClone
+//
+//  Created by Aliaksei Mikhailouski on 18/12/2022.
+//
+
+import SwiftUI
+
+class TweetsFetcher: ObservableObject {
+    @Published var tweets = [Tweet.noTweets]
+
+    let urlString = "http://localhost:8080/tweets"
+    
+    enum FetchError: Error {
+            case badRequest
+            case badJSON
+        }
+    
+    func fetchData() async
+    throws  {
+       guard let url = URL(string: urlString) else { return }
+
+       let (data, response) = try await URLSession.shared.data(for: URLRequest(url: url))
+       guard (response as? HTTPURLResponse)?.statusCode == 200 else { throw FetchError.badRequest }
+
+       Task { @MainActor in
+           do {
+               tweets = try JSONDecoder().decode([Tweet].self, from: data)
+               print(tweets)
+           } catch {
+               print(error)
+           }
+       }
+   }
+}
