4702b73c2f8b40ba2d8e7e761e2931e6beea8952
Alexey Mihailovsky <alexey.mihailovsky@gmail.com>
2022-12-18 13:38:51 +0100

    feat: add basic Tweet model

---
+++ TwitterClone/Tweet.swift
@@ -0,0 +1,13 @@
+//
+//  Tweet.swift
+//  TwitterClone
+//
+//  Created by Aliaksei Mikhailouski on 18/12/2022.
+//
+
+import SwiftUI
+
+struct Tweet: Codable {
+    var message: String
+    var userID: Int64
+}
