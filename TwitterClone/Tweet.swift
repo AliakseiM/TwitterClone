a8bfd17a2856d046fc5c61a1c352400aa4dbcc94
Alexey Mihailovsky <alexey.mihailovsky@gmail.com>
2022-12-18 21:46:28 +0100

    feat: modify Tweet model to be identifiable and to correspond to API model

--- TwitterClone/Tweet.swift
+++ TwitterClone/Tweet.swift
@@ -7,7 +7,20 @@
 
 import SwiftUI
 
-struct Tweet: Codable {
+struct Tweet: Codable, Identifiable {
+    var id: Int64 {
+            return messageID
+        }
+    
+    var messageID: Int64
     var message: String
     var userID: Int64
+    
+    static let noTweets = Tweet(messageID: 0, message: "You have no tweets", userID: 0)
+    
+    private enum CodingKeys: String, CodingKey {
+        case messageID = "message_id"
+        case message
+        case userID = "user_id"
+    }
 }
